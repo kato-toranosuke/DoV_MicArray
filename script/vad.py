2d8cbfbf4eba82f7a9ccee287674d1088e4a5d52
kato-toranosuke-mbp <toranosuke11041999@gmail.com>
2021-11-08 12:26:33 +0900

    [M] 有効区間の探知間隔の縮小

--- script/vad.py
+++ script/vad.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 # coding: utf-8
+
+from mylib.usb_4_mic_array.tuning import Tuning
+import usb.core
+import usb.util
+import time
+
+dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
+# print(dev)
+if dev:
+    Mic_tuning = Tuning(dev)
+    print(Mic_tuning.is_voice())
+    while True:
+        try:
+            print(Mic_tuning.is_voice())
+            time.sleep(0.1)
+        except KeyboardInterrupt:
+            break
